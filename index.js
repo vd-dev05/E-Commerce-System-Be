320b4bbba939776563c43db45e4b3cb4c36784d9
duonghoccodefullstack <dbui99620.com>
2025-03-01 23:27:43 +0700

    update options express

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://e-com-system.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://e-com-system.netlify.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
+//     if (req.method === 'OPTIONS') {
+//         return res.status(200).end();
+//     }
 
-    next();
-});
+//     next();
+// });
 app.use(cors({
     origin: 'e-com-system.netlify.app',
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
