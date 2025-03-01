c96463ca11c5b14f84be728a069a63c087f52d99
duonghoccodefullstack <dbui99620.com>
2025-03-01 22:54:54 +0700

    update options express

--- index.js
+++ index.js
@@ -17,26 +17,38 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-    origin: 'e-com-system.netlify.app',
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "Cache-Control",
-        "Expires",
-        "Pragma",
-    ],
-    credentials: true,
-}));
-app.options("*", cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://e-com-system.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    next();
+});
+// app.use(cors({
+//     origin: 'e-com-system.netlify.app',
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: [
+//         "Content-Type",
+//         "Authorization",
+//         "Cache-Control",
+//         "Expires",
+//         "Pragma",
+//     ],
+//     credentials: true,
+// }));
+// app.options("*", cors());
 app.use(cookieParser());
 
 // Route mặc định
 app.get("/", async (req, res) => {
     res.send('API Working');
- 
-    
+
+
 });
 
 app.use(RootRouter);
