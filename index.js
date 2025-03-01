9c017a9998f4b33d5bde2b6a3a4daa5408a2d329
duonghoccodefullstack <dbui99620.com>
2025-03-01 23:33:37 +0700

    update options express

--- index.js
+++ index.js
@@ -17,31 +17,31 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://e-com-system.netlify.app');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://e-com-system.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-//     if (req.method === 'OPTIONS') {
-//         return res.status(200).end();
-//     }
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
-//     next();
-// });
-app.use(cors({
-    origin: 'e-com-system.netlify.app',
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-        "Content-Type",
-        "Authorization",
-        "Cache-Control",
-        "Expires",
-        "Pragma",
-    ],
-    credentials: true,
-}));
-app.options("*", cors());
+    next();
+});
+// app.use(cors({
+//     origin: '*',
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: [
+//         "Content-Type",
+//         "Authorization",
+//         "Cache-Control",
+//         "Expires",
+//         "Pragma",
+//     ],
+//     credentials: true,
+// }));
+// app.options("*", cors());
 app.use(cookieParser());
 
 // Route mặc định
@@ -52,7 +52,7 @@ app.get("/", async (req, res) => {
 });
 
 app.use(RootRouter);
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port: ${port}`);
 
 });
