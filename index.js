c72cf090c92b470dc9a23bc9c648097a1f06fff0
duonghoccodefullstack <dbui99620.com>
2025-03-02 10:51:09 +0700

    update options express

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-
+    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
