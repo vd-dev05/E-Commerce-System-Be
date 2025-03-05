ad2d1d24b0ec2db5270aac05db304733eeaa9382
duonghoccodefullstack <dbui99620.com>
2025-03-05 20:22:46 +0700

    update

--- index.js
+++ index.js
@@ -30,24 +30,17 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use( (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', `${process.env.BASE_URL_FE}`);
+app.use(cookieParser());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.BASE_URL_FE);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
     next();
 });
-
-app.use(cors({
-    origin: process.env.BASE_URL_FE,
-    // methods: ["GET", "POST", "DELETE", "PUT"],
-    // allowedHeaders: [
-    //     "Content-Type",
-    //     "Authorization",
-    //     "Cache-Control",
-    //     "Expires",
-    //     "Pragma",
-    // ],
-    credentials: true,
-}));
-
 
 // Route mặc định
 app.get("/", async (req, res) => {
